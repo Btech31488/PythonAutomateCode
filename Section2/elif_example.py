75c82ec4d5f5d8c2f0f31e28f995a8ab1827b0f9
Brentsen Smith <brentsen.s@outlook.com>
2022-09-02 17:43:53 -0500

    This section has if, else, elif, while loops and for loops examples

---
+++ Section2/elif_example.py
@@ -0,0 +1,11 @@
+name = "Bob"
+age = 3000
+
+if name == "Alice":
+    print("Hi Alice")
+elif age < 12:
+    print("You are not Alice, kiddo")
+elif age > 2000:
+    print("So, you a vampire")
+elif age > 100:
+    print("Eh, you are more like a grannie")
